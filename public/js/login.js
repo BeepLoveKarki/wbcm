9a20b45b38bd1ad1b8a3e9c67098551faca107c2
Biplab <biplabkarki04@gmail.com>
2018-10-02 20:22:49 +0545

    Ought to complete

--- public/js/login.js
+++ public/js/login.js
@@ -3,5 +3,17 @@ function showup(){
   $(".sup").show();
   $(".txt1").empty();
   $(".txt1").text("Already have an account?");
-  $(".txt2").text("Sign In");
+  $(".umma").hide();
+  $(".umm").hide();
+  $(".umm1").show();
+}
+
+function showin(){
+  $(".sup").hide();
+  $(".sin").show();
+  $(".txt1").empty();
+  $(".txt1").text("Create an account?");
+  $(".umma").show();
+  $(".umm").show();
+  $(".umm1").hide();
 }
